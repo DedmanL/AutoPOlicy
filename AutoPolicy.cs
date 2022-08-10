d727023c3409d4b9504d6234192f5ff25c1f92b5
Lars Dedman <105458848+DedmanL@users.noreply.github.com>
2022-08-10 00:57:06 -0600

    Add files via upload

---
+++ AutoPolicy.cs
@@ -0,0 +1,28 @@
+﻿class AutoPolicy
+{
+    public int AccountNumber { get; set; }
+    public string MakeAndModel { get; set; }
+    public string State { get; set; }
+    public AutoPolicy(int AccountNumber, string MakeAndModel, string State)
+    {
+        AccountNumber =   accountNumber;
+        MakeAndModel = makeAndModel;
+        State = state;
+    }
+
+    public bool IsNoFaultState
+    {
+        get
+        {
+            bool noFaultState;
+
+            switch (State)
+            {
+                case CT: case MA: case ME: case NH: case NJ: case NY: case PA: case VT:
+                    noFaultState = true; break;
+                    default: noFaultState = false; break;
+            }
+            return noFaultState;
+        }
+    }
+}
